5dcfd6306552c2499ae116781a8f2b329b60c503
Nathan Lapinski <n4t311@gmail.com>
2018-06-01 08:44:42 +0900

    removing ts dependency

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
-"use strict";
-exports.__esModule = true;
+const { Observable, Subject, ReplaySubject, from, of, range } = require('rxjs');
+const { map, filter, switchMap } = require('rxjs/operators');
+
+range(1, 200)
+  .pipe(filter(x => x % 2 === 1), map(x => x + x))
+  .subscribe(x => console.log(x));
--- index.ts
+++
@@ -1,5 +0,0 @@
-import { Observable, Subject, asapScheduler, pipe, of, from, interval, merge, fromEvent } from 'rxjs';
-import { map, filter, scan } from 'rxjs/operators';
-import { ajax } from 'rxjs/ajax';
-
-
