ceb840dbbecca0856e6b75b4a819c58f4e2734a1
Nathan Lapinski <n4t311@gmail.com>
2018-06-06 19:16:24 +0900

    updating index.js

--- index.js
+++ index.js
@@ -4,3 +4,5 @@ const { map, filter, switchMap } = require('rxjs/operators');
 range(1, 200)
   .pipe(filter(x => x % 2 === 1), map(x => x + x))
   .subscribe(x => console.log(x));
+
+// add more examples
